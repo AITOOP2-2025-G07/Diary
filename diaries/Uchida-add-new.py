333d67bd896103ca90d4207e3f1033d45da6488d
MONSTER0129 <haruku0129@gmail.com>
2025-10-16 15:31:52 +0900

    Delete Uchida-add-new.py

--- diaries/Uchida-add-new.py
+++
@@ -1,9 +0,0 @@
-from diaries.AbstractDiary import AbstractDiary
-
-class UchidaDiary(AbstractDiary):
-    def get_date(self):
-        return "2025-10-16"
-    def get_summary(self):
-        return """Github難しい"""
-    def get_author(self):
-        return "Uchida"
