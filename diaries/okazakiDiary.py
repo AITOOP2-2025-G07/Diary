7d1428fd9924a66dabc35f78bd53415ecd155423
zzzak11 <hayato091110@icloud.com>
2025-10-22 09:36:17 +0900

    何もない１日だった->むずい

--- diaries/okazakiDiary.py
+++ diaries/okazakiDiary.py
@@ -4,6 +4,6 @@ class okazakiDiary(AbstractDiary):
     def get_date(self):
         return "2021-12-01"
     def get_summary(self):
-        return "なにもない一日だった"
+        return "むずい"
     def get_author(self):
-        return "Sample"
+        return "okazaki"
